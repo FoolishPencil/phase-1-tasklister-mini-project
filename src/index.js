4bec1662c532b628e365e3119aa064ba49d68c07
daniel lance <poorfoolspencil@gmail.com>
2024-10-21 19:58:31 -0500

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+
+    const taskInput = document.getElementById('new-task-description').value;
+
+
+    if (taskInput.trim() !== "") {
+      const taskList = document.getElementById('tasks');
+
+      const taskItem = document.createElement('li');
+      taskItem.textContent = taskInput;
+
+      taskList.appendChild(taskItem);
+      form.reset();
+    }
+  });
 });
